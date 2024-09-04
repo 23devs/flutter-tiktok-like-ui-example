b212d3074f25b4d8d84538665693c5d6fafd416e
maria.kudinova <maria.kudinova@office.e2e4gu.ru>
2024-09-04 19:37:33 +0700

    initial

---
+++ lib/features/authentication/views/sign_up_screen.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
+import 'package:impetus_test/constants/gaps.dart';
+import 'package:impetus_test/constants/sizes.dart';
+import 'package:impetus_test/features/authentication/views/login_screen.dart';
+import 'package:impetus_test/features/authentication/views/username_screen.dart';
+import 'package:impetus_test/features/authentication/views/widgets/auth_button.dart';
+import 'package:impetus_test/generated/l10n.dart';
+
+import '../../../common/widgets/dark_config/dark_mode_config.dart';
+import '../../../utils.dart';
+
+class SignUpScreen extends ConsumerWidget {
+  static const routeURL = "/";
+  static const routeName = "signUp";
+
+  const SignUpScreen({super.key});
+
+  void _onLoginTap(BuildContext context) async {
+    context.pushNamed(LoginScreen.routeName);
+  }
+
+  void _onEmailTap(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => const UsernameScreen(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        // if (orientation == Orientation.landscape) {
+        //   return const Scaffold(
+        //     body: Center(
+        //       child: Text("Please use portrait mode"),
+        //     ),
+        //   );
+        // }
+        return Scaffold(
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: Sizes.size40,
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Gaps.v80,
+                  const Text(
+                    "Sign up for Impetus",
+                    style: TextStyle(
+                      fontSize: Sizes.size24,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Gaps.v20,
+                  Opacity(
+                    opacity: 0.7,
+                    child: Text(
+                      S.of(context).signUpSubtitle(11),
+                      style: const TextStyle(
+                        fontSize: Sizes.size16,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Gaps.v40,
+                  if (orientation == Orientation.portrait) ...[
+                    AuthButton(
+                      icon: const FaIcon(FontAwesomeIcons.user),
+                      // text: S.of(context).emailPasswordButton,
+                      text: "Use email & password",
+                      onTap: _onEmailTap,
+                    ),
+                    Gaps.v16,
+                    const ConsumerAuthButton(
+                      icon: FaIcon(FontAwesomeIcons.github),
+                      text: "Continue with Github",
+                      onTap: githubSignIn,
+                    ),
+                  ],
+                  if (orientation == Orientation.landscape)
+                    Row(
+                      children: [
+                        Expanded(
+                          child: AuthButton(
+                            icon: const FaIcon(FontAwesomeIcons.user),
+                            // text: S.of(context).emailPasswordButton,
+                            text: "Use email & password",
+                            onTap: _onEmailTap,
+                          ),
+                        ),
+                        Gaps.h16,
+                        Expanded(
+                          child: AuthButton(
+                            icon: const FaIcon(FontAwesomeIcons.apple),
+                            text: S.of(context).appleButton,
+                          ),
+                        ),
+                      ],
+                    ),
+                ],
+              ),
+            ),
+          ),
+          bottomNavigationBar: Container(
+            color: darkModeConfig.value ? null : Colors.grey.shade50,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: Sizes.size32,
+                bottom: Sizes.size64,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    S.of(context).alreadyHaveAnAccount,
+                    style: const TextStyle(
+                      fontSize: Sizes.size16,
+                    ),
+                  ),
+                  Gaps.h5,
+                  GestureDetector(
+                    onTap: () => _onLoginTap(context),
+                    child: Text(
+                      // S.of(context).logIn("female"),
+                      "Log in",
+                      style: TextStyle(
+                        fontSize: Sizes.size16,
+                        fontWeight: FontWeight.w600,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
